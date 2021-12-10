ec4a647afb0b439818653acf6b3ea73e3db36f8c
Rod Morgado <rodrigo@zooft.company>
2021-12-10 13:12:27 -0800

    using predict() to find values from different explanatory data created in the lm model

--- first-chapter.R
+++ first-chapter.R
@@ -29,3 +29,15 @@ mdl_price_vs_age_no_intercept <- lm(
 
 # See the result
 mdl_price_vs_age_no_intercept
+
+# From previous steps
+explanatory_data <- tibble(
+  n_convenience = 0:10
+)
+
+# Edit this, so predictions are stored in prediction_data
+prediction_data <- explanatory_data %>%
+  mutate(price_twd_msq = predict(mdl_price_vs_conv, explanatory_data))
+
+# See the result
+prediction_data
