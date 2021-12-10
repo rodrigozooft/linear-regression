f0fe07324dbaa9c725803cb3145d007483d053e3
Rod Morgado <rodrigo@zooft.company>
2021-12-10 12:58:56 -0800

    hisogram of the house prices according to houses ages

--- first-chapter.R
+++ first-chapter.R
@@ -4,3 +4,11 @@
 ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq)) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "lm", se = FALSE)
+
+# Using taiwan_real_estate, plot price_twd_msq
+taiwan_real_estate %>% ggplot(aes(x = price_twd_msq)) +
+  # Make it a histogram with 10 bins
+  geom_histogram(bins = 10) +
+  # Facet the plot so each house age group gets its own panel
+  facet_wrap(~ house_age_years)
+
