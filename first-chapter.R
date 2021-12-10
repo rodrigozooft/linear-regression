7ca2d42bc13679f9355da9d4b6bf147b8859f530
Rod Morgado <rodrigo@zooft.company>
2021-12-10 14:20:35 -0800

    running some predictions using return_2018 new valuess

--- first-chapter.R
+++ first-chapter.R
@@ -79,4 +79,14 @@ sp500_yearly_returns %>%
   # Fix the coordinate ratio
   coord_fixed()
 
+# From previous step
+mdl_returns <- lm(
+  return_2019 ~ return_2018, 
+  data = sp500_yearly_returns
+)
 
+# Create a data frame with return_2018 at -1, 0, and 1 
+explanatory_data <- tibble(return_2018 = c(-1, 0, 1))
+
+# Use mdl_returns to predict with explanatory_data
+predict(mdl_returns, explanatory_data)
