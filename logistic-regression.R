c835f8cae1bb10b4b1dbb2a73990282837d8c0f0
Rod Morgado <rodrigo@zooft.company>
2021-12-13 15:47:10 -0800

    using geom_smooth with method "glm" and method.args with a list(family= binomial)

--- logistic-regression.R
+++ logistic-regression.R
@@ -5,3 +5,19 @@ churn %>%
     ggplot(aes(x = time_since_first_purchase)) + 
     geom_histogram(binwidth = 0.25) + 
     facet_grid(rows = vars(has_churned))
+
+# Using churn plot has_churned vs. time_since_first_purchase
+  ggplot(churn, aes(x = time_since_first_purchase, y = has_churned)) +
+  # Make it a scatter plot
+  geom_point() +
+  # Add an lm trend line, no std error ribbon, colored red
+  geom_smooth(method = "lm", color = "red", se = FALSE)
+
+
+ggplot(churn, aes(time_since_first_purchase, has_churned)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  # Add a glm trend line, no std error ribbon, binomial family
+  geom_smooth(method = "glm", se = FALSE, method.args = list(family = binomial))
+
+
