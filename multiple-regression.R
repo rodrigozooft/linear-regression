7b11ebea426f309c611da69829a85e437cc004d8
Rod Morgado <rodrigo@zooft.company>
2021-12-14 14:55:36 -0800

    using geom_parallel_slopes() to apply a linear model to three different categories

--- multiple-regression.R
+++ multiple-regression.R
@@ -5,3 +5,11 @@ mdl_price_vs_both <- lm(formula = price_twd_msq ~ n_convenience + house_age_year
 
 # See the result
 mdl_price_vs_both
+
+# Using taiwan_real_estate, plot price_twd_msq vs. n_convenience
+# colored by house_age_years
+ggplot(taiwan_real_estate, aes(y = price_twd_msq, x = n_convenience, color = house_age_years)) +
+  # Add a point layer
+  geom_point() +
+  # Add parallel slopes, no ribbon
+  geom_parallel_slopes(se = FALSE)
