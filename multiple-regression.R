e357723597c6ac5804c9c50029316579d886afb6
Rod Morgado <rodrigo@zooft.company>
2021-12-14 15:42:18 -0800

    adding a prediction column using the different models available

--- multiple-regression.R
+++ multiple-regression.R
@@ -114,3 +114,26 @@ mdl_30_to_45 <- lm(price_twd_msq ~ n_convenience, data = taiwan_30_to_45)
 mdl_0_to_15
 mdl_15_to_30
 mdl_30_to_45
+
+# From previous step
+explanatory_data <- tibble(
+  n_convenience = 0:10
+)
+
+# Add column of predictions using "0 to 15" model and explanatory data 
+prediction_data_0_to_15 <- explanatory_data %>% 
+  mutate(
+    predict(mdl_0_to_15, explanatory_data)
+  )
+
+# Same again, with "15 to 30"
+prediction_data_15_to_30 <- explanatory_data %>% 
+  mutate(
+    predict(mdl_15_to_30, explanatory_data)
+  )
+
+# Same again, with "30 to 45"
+prediction_data_30_to_45 <- explanatory_data %>% 
+  mutate(
+    predict(mdl_30_to_45, explanatory_data)
+  )
