b711bf1dd7506df4d2bb5093c1bbfde716be58ca
Rod Morgado <rodrigo@zooft.company>
2021-12-14 15:10:39 -0800

    performing some predictions using a model built with two variables

--- multiple-regression.R
+++ multiple-regression.R
@@ -13,3 +13,24 @@ ggplot(taiwan_real_estate, aes(y = price_twd_msq, x = n_convenience, color = hou
   geom_point() +
   # Add parallel slopes, no ribbon
   geom_parallel_slopes(se = FALSE)
+
+# From previous steps
+explanatory_data <- expand_grid(
+  n_convenience = 0:10,
+  house_age_years = unique(taiwan_real_estate$house_age_years)
+)
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_both, explanatory_data)
+  )
+
+taiwan_real_estate %>% 
+  ggplot(aes(n_convenience, price_twd_msq, color = house_age_years)) +
+  geom_point() +
+  geom_parallel_slopes(se = FALSE) +
+  # Add points using prediction_data, with size 5 and shape 15
+  geom_point(
+    data = prediction_data,
+    size = 5,
+    shape = 15
+  )
