adb1fc93f07b7f7287ab61c9d6a1f3a2110a4bb7
Rod Morgado <rodrigo@zooft.company>
2021-12-14 15:36:16 -0800

    running models after splitting the dataset in different categories:w

--- multiple-regression.R
+++ multiple-regression.R
@@ -92,3 +92,25 @@ mdl_price_vs_both %>%
   glance() %>% 
   # Pull out the RSE
   pull(sigma)
+
+# From previous step
+taiwan_0_to_15 <- taiwan_real_estate %>%
+  filter(house_age_years == "0 to 15")
+taiwan_15_to_30 <- taiwan_real_estate %>%
+  filter(house_age_years == "15 to 30")
+taiwan_30_to_45 <- taiwan_real_estate %>%
+  filter(house_age_years == "30 to 45")
+
+# Model price vs. no. convenience stores using 0 to 15 data
+mdl_0_to_15 <- lm(price_twd_msq ~ n_convenience, data = taiwan_0_to_15)
+
+# Model price vs. no. convenience stores using 15 to 30 data
+mdl_15_to_30 <- lm(price_twd_msq ~ n_convenience, data = taiwan_15_to_30)
+
+# Model price vs. no. convenience stores using 30 to 45 data
+mdl_30_to_45 <- lm(price_twd_msq ~ n_convenience, data = taiwan_30_to_45)
+
+# See the results
+mdl_0_to_15
+mdl_15_to_30
+mdl_30_to_45
