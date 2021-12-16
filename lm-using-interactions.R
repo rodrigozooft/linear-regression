f493d32f77743fe52f95f977317c1ecbe1680705
Rod Morgado <rodrigo@zooft.company>
2021-12-15 16:20:49 -0800

    making the predictions manually using the slope and intercept for the right house_years category

--- lm-using-interactions.R
+++ lm-using-interactions.R
@@ -2,3 +2,47 @@
 
 # Model price vs both with an interaction using "colon" syntax
 lm(price_twd_msq ~ n_convenience + house_age_years + n_convenience:house_age_years, data = taiwan_real_estate)
+
+# From previous step
+explanatory_data <- expand_grid(
+  n_convenience = 0:10,
+  house_age_years = unique(taiwan_real_estate$house_age_years)
+)
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_both_inter, explanatory_data)
+  )
+
+# 
+taiwan_real_estate %>%
+  ggplot(aes(x = n_convenience, y = price_twd_msq, color = house_age_years)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = FALSE) + 
+  geom_point(
+    data = prediction_data,
+    size = 5,
+    shape = 15
+  )
+
+# From previous step
+coeffs <- coefficients(mdl_price_vs_both_inter)
+intercept_0_15 <- coeffs[1]
+intercept_15_30 <- coeffs[2]
+intercept_30_45 <- coeffs[3]
+slope_0_15 <- coeffs[4]
+slope_15_30 <- coeffs[5]
+slope_30_45 <- coeffs[6]
+
+prediction_data <- explanatory_data %>% 
+  mutate(
+    # Consider the 3 cases to choose the price
+    price_twd_msq = case_when(
+      house_age_years == "0 to 15" ~ intercept_0_15 + slope_0_15 * n_convenience,
+      house_age_years == "15 to 30" ~ intercept_15_30 + slope_15_30 * n_convenience,
+      house_age_years == "30 to 45" ~ intercept_30_45 + slope_30_45 * n_convenience
+    )
+  )
+
+# See the result
+prediction_data
+
