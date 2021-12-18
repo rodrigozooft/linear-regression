92d9e7c604d659c0f97f6ea7ae2756ca89bf88df
Rod Morgado <rodrigo@zooft.company>
2021-12-17 16:30:35 -0800

    solve the minimum of the function using algorithms

--- multiple-logistic-regression.R
+++ multiple-logistic-regression.R
@@ -69,3 +69,23 @@ all.equal(
     linkfun_p,
     qlogis(p)
 )
+
+# From previous step
+calc_neg_log_likelihood <- function(coeffs) {
+  intercept <- coeffs[1]
+  slope <- coeffs[2]
+  y_pred <- plogis(intercept + slope * x_actual)
+  log_likelihoods <- log(y_pred) * y_actual + log(1 - y_pred) * (1 - y_actual)
+  -sum(log_likelihoods)
+}
+
+# Optimize the metric
+optim(
+  # Initially guess 0 intercept and 1 slope
+  par = c(intercept = 0, slope = 1),
+  # Use calc_neg_log_likelihood as the optimization fn 
+  fn = calc_neg_log_likelihood
+)
+
+# Compare the coefficients to those calculated by glm()
+glm(has_churned ~ time_since_last_purchase, data = churn, family = binomial)
