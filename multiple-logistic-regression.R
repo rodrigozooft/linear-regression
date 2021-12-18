d7694085ed2839acc69ec922b1e70f027ebd7872
Rod Morgado <rodrigo@zooft.company>
2021-12-17 16:09:20 -0800

    working with PDF and CDF to understand to work with numbers and probabilities

--- multiple-logistic-regression.R
+++ multiple-logistic-regression.R
@@ -48,3 +48,24 @@ logistic_distn_inv_cdf <- tibble(
 ggplot(logistic_distn_inv_cdf, aes(y = logit_p, x = p)) +
   # Make it a line plot
   geom_line()
+
+# Look at the structure of binomial() function
+str(binomial())
+
+# Call the link inverse on x
+linkinv_x <- binomial()$linkinv(x)
+
+# Check linkinv_x and plogis() of x give same results 
+all.equal(
+    linkinv_x,
+    plogis(x)
+)
+
+# Call the link fun on p
+linkfun_p <- binomial()$linkfun(p)
+
+# Check linkfun_p and qlogis() of p give same results  
+all.equal(
+    linkfun_p,
+    qlogis(p)
+)
