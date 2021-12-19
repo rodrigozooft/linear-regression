da7909d7ab3109f41c01ebe165af09cd564144c2
Rod Morgado <rodrigo@zooft.company>
2021-12-18 21:05:47 -0800

    using different K's to check the performance of the classifier

--- knn-machilne-learning.R
+++ knn-machilne-learning.R
@@ -8,3 +8,15 @@ table(signs_pred, signs_actual)
 
 # Compute the accuracy
 mean(signs_pred == signs_actual)
+
+# Compute the accuracy of the baseline model (default k = 1)
+k_1 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types)
+mean(signs_actual == k_1)
+
+# Modify the above to set k = 7
+k_7 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k = 7)
+mean(signs_actual == k_7)
+
+# Set k = 15 and compare to the above
+k_15 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k = 15)
+mean(signs_actual == k_15)
