84260d45b0a9609ef16409029dc3b2f1a59759bc
Rod Morgado <rodrigo@zooft.company>
2021-12-21 15:22:38 -0800

    a way of handling missing value imputing a rounded average calculation

--- factor-in-regression.R
+++ factor-in-regression.R
@@ -6,3 +6,12 @@ donors$wealth_levels <- relevel(donors$wealth_levels, ref = "Medium")
 
 # See how our factor coding impacts the model
 summary(glm(donated ~ wealth_levels, family = "binomial", data = donors))
+
+# Find the average age among non-missing values
+summary(donors$age)
+
+# Impute missing age values with the mean age
+donors$imputed_age <- ifelse(is.na(donors$age), round(mean(donors$age, na.rm = TRUE), 2), donors$age)
+
+# Create missing value indicator for age
+donors$missing_age <- ifelse(is.na(donors$age), 1, 0)
