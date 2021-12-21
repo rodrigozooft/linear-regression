ed2f3b433a8038284b330f5fcc1af9a38aa3206a
Rod Morgado <rodrigo@zooft.company>
2021-12-21 15:43:48 -0800

    running a model with none explanatory variables to use step wise method to define the final model

--- factor-in-regression.R
+++ factor-in-regression.R
@@ -15,3 +15,36 @@ donors$imputed_age <- ifelse(is.na(donors$age), round(mean(donors$age, na.rm = T
 
 # Create missing value indicator for age
 donors$missing_age <- ifelse(is.na(donors$age), 1, 0)
+
+# Build a recency, frequency, and money (RFM) model
+rfm_model <- glm(formula = donated ~ money + recency * frequency, family = "binomial", data = donors)
+
+# Summarize the RFM model to see how the parameters were coded
+summary(rfm_model)
+
+# Compute predicted probabilities for the RFM model
+rfm_prob <- predict(rfm_model, donors, type = "response")
+
+# Plot the ROC curve and find AUC for the new model
+library(pROC)
+ROC <- roc(donors$donated, rfm_prob)
+plot(ROC, col = "red")
+auc(ROC)
+
+# Specify a null model with no predictors
+null_model <- glm(donated ~ 1, data = donors, family = "binomial")
+
+# Specify the full model using all of the potential predictors
+full_model <- glm(donated ~ ., data = donors, family = "binomial")
+
+# Use a forward stepwise algorithm to build a parsimonious model
+step_model <- step(null_model, scope = list(lower = null_model, upper = full_model), direction = "forward")
+
+# Estimate the stepwise donation probability
+step_prob <- predict(step_model, donors, type = "response")
+
+# Plot the ROC of the stepwise model
+library(pROC)
+ROC <- roc(donors$donated, step_prob)
+plot(ROC, col = "red")
+auc(ROC)
