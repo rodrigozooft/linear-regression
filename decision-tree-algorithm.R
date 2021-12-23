20da105408e18a2da5c0d5c07ac9d78b60f9a3aa
Rod Morgado <rodrigo@zooft.company>
2021-12-22 21:13:21 -0800

    using the two datasets a decision tree model is trained and compared its performance

--- decision-tree-algorithm.R
+++ decision-tree-algorithm.R
@@ -33,3 +33,15 @@ loans_train <- loans[sample_rows, ]
 
 # Create the test dataset
 loans_test <- loans[-sample_rows, ]
+
+# Grow a tree using all of the available applicant data
+loan_model <- rpart(formula = outcome ~ ., data = loans_train, method = "class", control = rpart.control(cp = 0))
+
+# Make predictions on the test dataset
+loans_test$pred <- predict(loan_model, loans_test, type = "class")
+
+# Examine the confusion matrix
+table(loans_test$pred, loans_test$outcome)
+
+# Compute the accuracy on the test dataset
+mean(loans_test$pred == loans_test$outcome)
