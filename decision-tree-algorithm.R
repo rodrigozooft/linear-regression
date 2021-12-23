b6155b24e45d3a0645939a9f482cbfde49c3ca77
Rod Morgado <rodrigo@zooft.company>
2021-12-22 20:24:49 -0800

    using rpart.plot to visualize how the decision tree was built

--- decision-tree-algorithm.R
+++ decision-tree-algorithm.R
@@ -9,3 +9,15 @@ predict(loan_model, good_credit, type = "class")
 
 # Make a prediction for someone with bad credit
 predict(loan_model, bad_credit, type = "class")
+
+# Examine the loan_model object
+loan_model
+
+# Load the rpart.plot package
+library(rpart.plot)
+
+# Plot the loan_model with default settings
+rpart.plot(loan_model)
+
+# Plot the loan_model with customized settings
+rpart.plot(loan_model, type = 3, box.palette = c("red", "green"), fallen.leaves = TRUE)
