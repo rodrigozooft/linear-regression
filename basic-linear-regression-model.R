d8ef4ad48a15476dd94534e3b10e9632082ca5bf
Rod Morgado <rodrigo@zooft.company>
2021-12-23 15:59:37 -0800

    there are four ways to explore your model. Just print the model, using summary, glance and wrapFTest

--- basic-linear-regression-model.R
+++ basic-linear-regression-model.R
@@ -12,3 +12,16 @@ unemployment_model <- lm(formula = fmla, data = unemployment)
 
 # Print it
 unemployment_model
+
+# broom and sigr are already loaded in your workspace
+# Print unemployment_model
+unemployment_model
+
+# Call summary() on unemployment_model to get more details
+summary(unemployment_model)
+
+# Call glance() on unemployment_model to see the details in a tidier form
+glance(unemployment_model)
+
+# Call wrapFTest() on unemployment_model to see the most relevant details
+wrapFTest(unemployment_model)
