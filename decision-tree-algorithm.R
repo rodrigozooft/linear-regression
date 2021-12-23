dff7f85db59e911a6be7f7324c68464a25c0a13f
Rod Morgado <rodrigo@zooft.company>
2021-12-22 21:23:40 -0800

    applying pre and post prune to improve the quality of the model

--- decision-tree-algorithm.R
+++ decision-tree-algorithm.R
@@ -45,3 +45,10 @@ table(loans_test$pred, loans_test$outcome)
 
 # Compute the accuracy on the test dataset
 mean(loans_test$pred == loans_test$outcome)
+
+# Swap maxdepth for a minimum split of 500 
+loan_model <- rpart(outcome ~ ., data = loans_train, method = "class", control = rpart.control(cp = 0, minsplit = 500))
+
+# Run this. How does the accuracy change?
+loans_test$pred <- predict(loan_model, loans_test, type = "class")
+mean(loans_test$pred == loans_test$outcome)
