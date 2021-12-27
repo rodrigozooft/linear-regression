cd236aeb264255dfc61cb89c499c2dc1df15ece0
Rod Morgado <rodrigo@zooft.company>
2021-12-27 12:18:28 -0800

    using r squared to see how well the moodel fits the data, using 1 - rss/tss how much variance is explained by the model

--- basic-linear-regression-model.R
+++ basic-linear-regression-model.R
@@ -111,3 +111,24 @@ res <- unemployment$residuals
 
 # Calculate the standard deviation of female_unemployment and print it
 (sd_unemployment <- sd(unemployment$female_unemployment))
+
+# unemployment is in your workspace
+summary(unemployment)
+
+# unemployment_model is in the workspace
+summary(unemployment_model)
+
+# Calculate mean female_unemployment: fe_mean. Print it
+(fe_mean <- mean(unemployment$female_unemployment))
+
+# Calculate total sum of squares: tss. Print it
+(tss <- sum((unemployment$female_unemployment - fe_mean)^2))
+
+# Calculate residual sum of squares: rss. Print it
+(rss <- sum(unemployment$residuals ^ 2))
+
+# Calculate R-squared: rsq. Print it. Is it a good fit?
+(rsq <- 1 - rss / tss)
+
+# Get R-squared from glance. Print it
+(rsq_glance <- glance(unemployment_model)$r.squared)
