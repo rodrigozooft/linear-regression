48af79cf7b90dc980e1478852f548c65b0fd2e66
Rod Morgado <rodrigo@zooft.company>
2021-12-27 11:57:45 -0800

    calculating the RMSE and comparing the value to the sd

--- basic-linear-regression-model.R
+++ basic-linear-regression-model.R
@@ -99,3 +99,15 @@ library(WVPlots)
 
 # Plot the Gain Curve
 GainCurvePlot(unemployment, "predictions", "female_unemployment", "Unemployment model")
+
+# unemployment is in the workspace
+summary(unemployment)
+
+# For convenience put the residuals in the variable res
+res <- unemployment$residuals
+
+# Calculate RMSE, assign it to the variable rmse and print it
+(rmse <- sqrt(mean(res ^ 2)))
+
+# Calculate the standard deviation of female_unemployment and print it
+(sd_unemployment <- sd(unemployment$female_unemployment))
