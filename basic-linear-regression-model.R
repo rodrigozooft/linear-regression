dff130c1d99822da2e174b395b47254bd5bd2f99
Rod Morgado <rodrigo@zooft.company>
2021-12-27 11:10:50 -0800

    using ggplot to assess model performance comparing the residuals

--- basic-linear-regression-model.R
+++ basic-linear-regression-model.R
@@ -75,3 +75,15 @@ bloodpressure$prediction <- predict(bloodpressure_model, bloodpressure)
 ggplot(bloodpressure, aes(x = prediction, y = blood_pressure)) + 
     geom_point() +
     geom_abline(color = "blue")
+
+# From previous step
+unemployment$predictions <- predict(unemployment_model)
+
+# Calculate residuals
+unemployment$residuals <- unemployment$female_unemployment - unemployment$predictions
+
+# Fill in the blanks to plot predictions (on x-axis) versus the residuals
+ggplot(unemployment, aes(x = predictions, y = residuals)) + 
+  geom_pointrange(aes(ymin = 0, ymax = residuals)) + 
+  geom_hline(yintercept = 0, linetype = 3) + 
+  ggtitle("residuals vs. linear model prediction")
