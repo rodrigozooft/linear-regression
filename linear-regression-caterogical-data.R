10f815552520e4063dfa966c8e463e57de057d75
Rod Morgado <rodrigo@zooft.company>
2021-12-28 10:51:20 -0800

    Understanding how R represents categorical data internally

---
+++ linear-regression-caterogical-data.R
@@ -0,0 +1,17 @@
+# Call str on flowers to see the types of each column
+str(flowers)
+
+# Use unique() to see how many possible values Time takes
+unique(flowers$Time)
+
+# Build a formula to express Flowers as a function of Intensity and Time: fmla. Print it
+(fmla <- as.formula("Flowers ~ Intensity + Time"))
+
+# Use fmla and model.matrix to see how the data is represented for modeling
+mmat <- model.matrix(fmla, flowers)
+
+# Examine the first 20 lines of flowers
+head(flowers, 20)
+
+# Examine the first 20 lines of mmat
+head(mmat, 20)
