e6faebb88ea7480bd9b5d362b0a5289ed00b36ba
Rod Morgado <rodrigo@zooft.company>
2021-12-28 10:55:50 -0800

    creating a lm model and predicting the number of flowers based on intensity and timeline of the light

--- linear-regression-caterogical-data.R
+++ linear-regression-caterogical-data.R
@@ -15,3 +15,26 @@ head(flowers, 20)
 
 # Examine the first 20 lines of mmat
 head(mmat, 20)
+
+# flowers in is the workspace
+str(flowers)
+
+# fmla is in the workspace
+fmla
+
+# Fit a model to predict Flowers from Intensity and Time : flower_model
+flower_model <- lm(fmla, flowers)
+
+# Use summary on mmat to remind yourself of its structure
+summary(mmat)
+
+# Use summary to examine flower_model 
+summary(flower_model)
+
+# Predict the number of flowers on each plant
+flowers$predictions <- predict(flower_model, flowers)
+
+# Plot predictions vs actual flowers (predictions on x-axis)
+ggplot(flowers, aes(x = predictions, y = Flowers)) + 
+  geom_point() +
+  geom_abline(color = "blue") 
